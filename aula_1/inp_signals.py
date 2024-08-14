f10250b854bda60c35c244ccc29b8f354a477664
SergioBerteli <sergio@univali.br>
2024-08-13 22:18:14 -0300

    step e impulso

--- aula_1/inp_signals.py
+++ aula_1/inp_signals.py
@@ -1,33 +1,50 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def degrau_unitario(inicio, fim):
-    sinal = [1 if i >= 0 else 0 for i in range(inicio, fim)]
-    return np.array(sinal)
+def degrau_unitario(n):
+    sinal = np.where(n>=0, 1, 0)
+    return sinal
 
-def impulso_unitario(inicio, fim):
-    sinal = [1 if i == 0 else 0 for i in range(inicio, fim)]
-    return np.array(sinal)
+def impulso_unitario(n):
+    sinal = np.where(n == 0, 1, 0)
+    return sinal
 
 def sinusoidal(n, f, ganho=1, fi=0):
     sinal = ganho*np.sin(2*np.pi*f*n + fi)
     return sinal
 
 if __name__ == "__main__":
-    degrau = degrau_unitario(-1, 4)
-    print(degrau)
-    impulso = impulso_unitario(-2, 2)
-    print(impulso)
-
-    inicio = -2
-    fim = 1
-    freq_amostragem = 800
-    ts = 1/freq_amostragem
+    # definição de parametros iniciais
+    f_samp = 100
+    ts = 1/f_samp
+    inicio = -0.1
+    fim = 0.1
     n = np.arange(inicio, fim, ts)
-    sin_teste = sinusoidal(n, 400)
+    n = np.round(n, decimals=12)
+    
+    # testes com o degrau
+    degrau = degrau_unitario(n)
+    
+    relu = n*degrau
+    
+    #testes com o impulso
+    
+    imp = impulso_unitario(n)
+    
+    imp_pi = np.pi * imp
+    
+    fig, ((ax_degrau, ax_relu), (ax_impulso, ax_pi_impulso), (ax_sinu, ax_sin_teste), (ax_exp, ax_exp_teste)) = plt.subplots(4, 2)
+    ax_degrau.stem(n, degrau)
+    ax_degrau.set_title("Função degrau")
 
-    fig, (ax1, ax2, ax3) = plt.subplots(3, 1)
-    ax1.stem(np.arange(-1, 4, 1), degrau)
-    ax2.stem(np.arange(-2, 2, 1), impulso)
-    ax3.stem(n, sin_teste)
+    ax_relu.stem(n, relu)
+    ax_relu.set_title("Função relu (step * x)")
+
+    ax_impulso.stem(n, imp)
+    ax_impulso.set_title("Função impulso")
+
+    ax_pi_impulso.stem(n, imp_pi)
+    ax_pi_impulso.set_title("Função impulso * pi")
+    
+    plt.subplots_adjust(hspace=0.5)
     plt.show()
